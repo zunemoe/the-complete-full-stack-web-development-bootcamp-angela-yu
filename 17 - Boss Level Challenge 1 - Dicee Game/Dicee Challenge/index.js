cefda8c1122bcbe5ddd504ff3a7ad8efbf946969
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-06-12 23:21:23 +1200

    Added Section 17 and 18

---
+++ 17 - Boss Level Challenge 1 - Dicee Game/Dicee Challenge/index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;    
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+document.querySelector(".btn").addEventListener("click", function() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;    
+    document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "Player 1 Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player 2 Wins!";
+    } else {
+        document.querySelector("h1").textContent = "Draw!";
+    }
+});

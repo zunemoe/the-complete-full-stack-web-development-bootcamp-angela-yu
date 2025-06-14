e312661519041e9842408fcd6e1a01d356272ff6
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-06-14 21:23:14 +1200

    Updated code for 24 - 3.3 Middleware

--- 24 - Express.js with Node.js/3.4 Middleware/index2.js
+++ 24 - Express.js with Node.js/3.4 Middleware/index2.js
@@ -1,7 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello");

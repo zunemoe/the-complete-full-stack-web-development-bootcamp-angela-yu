9c5f79ec0cf12619032539a70f5f56ea84a69463
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-06-15 18:40:06 +1200

    EJS - 4.2, 4.3, 4.4 codes

--- 25 - EJS/4.2 Passing Data/index.js
+++ 25 - EJS/4.2 Passing Data/index.js
@@ -6,9 +6,18 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+  console.log(req.body);
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  let length = req.body.fName.length + req.body.lName.length;
+  res.render("index.ejs", {
+    length: length,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

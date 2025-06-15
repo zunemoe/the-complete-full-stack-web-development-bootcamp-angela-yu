6d8177e0272adb856e6a1db5899584e3d3cc635c
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-06-15 21:11:44 +1200

    git merge

--- 24 - Express.js with Node.js/3.4 Middleware/index3.js
+++ 24 - Express.js with Node.js/3.4 Middleware/index3.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 const app = express();
 const port = 3000;
 
@@ -12,3 +11,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function logger(req, res, next) {
+  console.log(`Method: ${req.method} URL: ${req.url}`);
+  next();
+}
--- 24 - Express.js with Node.js/3.4 Middleware/index4.js
+++ 24 - Express.js with Node.js/3.4 Middleware/index4.js
@@ -1,8 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bandNameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(`<h1>Your band name is</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function bandNameGenerator(req, res, next) {
+  bandName = req.body.street + req.body.pet;
+  next();
+}

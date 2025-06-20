ecc5164df1863a39ab8042d6ac225b60ac588fbf
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-06-20 16:21:34 +1200

    28 - API -> 5.5 REST APIs npm audit fix

--- 28 - API/5.5 REST APIs/index.js
+++ 28 - API/5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "8032d02a-7234-4178-a96c-f0d6c59b7fbd";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
--- 28 - API/5.5 REST APIs/solution.js
+++ 28 - API/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "8032d02a-7234-4178-a96c-f0d6c59b7fbd";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
